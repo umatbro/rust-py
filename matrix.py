e4bb1f9b76d9ade19755f3b776726552c684e0e9
umatbro <umatbroo@gmail.com>
2023-06-11 15:04:52 +0200

    Step 1

---
+++ matrix.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+
+class PyMatrix:
+    def __init__(self, data: list[list[int]]):
+        if not all(len(row) == len(data[0]) for row in data):
+            raise ValueError("Invalid matrix dimensions. All rows must have the same length.")
+        self.rows = len(data)
+        self.cols = len(data[0])
+        self.data = data
+
+    def dot(self, other: PyMatrix):
+        if self.cols != other.rows:
+            raise ValueError(
+                "Invalid matrix dimensions. "
+                "The number of columns of the first matrix must be equal to the number of rows of the second matrix. "
+                f"Got {self.cols} and {other.rows} instead."
+            )
+
+        result = PyMatrix([[0 for _ in range(other.cols)] for _ in range(self.rows)])
+
+        for row_self in range(self.rows):
+            for col_other in range(other.cols):
+                for i in range(self.cols):
+                    result.data[row_self][col_other] += self.data[row_self][i] * other.data[i][col_other]
+
+        return result
---
+++ tests.py
@@ -0,0 +1,42 @@
+import pytest
+from matrix import PyMatrix
+
+
+@pytest.mark.parametrize(
+    ["a", "b", "expected"],
+    [
+        (
+            PyMatrix([
+                [1, 2, 3],
+                [4, 5, 6],
+            ]),
+            PyMatrix([
+                [7, 8],
+                [9, 10],
+                [11, 12],
+            ]),
+            PyMatrix([
+                [58, 64],
+                [139, 154],
+            ]),
+        ),
+        (
+            PyMatrix([
+                [3, 6, 9],
+                [2, 4, 6],
+            ]),
+            PyMatrix([
+                [1, 2],
+                [3, 4],
+                [5, 6],
+            ]),
+            PyMatrix([
+                [66, 84],
+                [44, 56],
+            ]),
+        )
+    ]
+)
+def test_python_matrix_mul(a, b, expected):
+    result = a.dot(b)
+    assert result.data == expected.data
